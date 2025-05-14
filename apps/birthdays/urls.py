890175beac0ab21ad36c64183d21a95f2f7f10f8
Dalan Weber <dalanweber@Dalans-MacBook-Air.local>
2025-05-14 14:42:32 -0600

    Added functionality to view birthday details and send messages to the user

--- apps/birthdays/urls.py
+++ apps/birthdays/urls.py
@@ -6,4 +6,7 @@ app_name = 'birthdays'
 urlpatterns = [
     path('', views.home, name='home'),
     path('add/', views.add_birthday, name='add'),
+    path('view/<int:id>/', views.view_birthday, name='view'),
+    path('edit/<int:id>/', views.edit_birthday, name='edit'),
+    path('delete/<int:id>/', views.delete_birthday, name='delete'),
 ]
--- apps/birthdays/views.py
+++ apps/birthdays/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from django.contrib import messages
 from django.contrib.auth.models import AnonymousUser
 from apps.birthdays.models import Birthday
 from .forms import BirthdayForm
@@ -20,7 +21,40 @@ def add_birthday(request):
             birthday = form.save(commit=False)
             birthday.user = request.user
             birthday.save()
+            messages.success(request, "Birthday added successfully!")
             return redirect('birthdays:add')
     else:
         form = BirthdayForm()
     return render(request, 'birthdays/add_birthday.html', {'form': form})
+
+def edit_birthday(request, id):
+    birthday =  Birthday.objects.get(id = id)
+
+    if request.method == 'POST':
+        form = BirthdayForm(request.POST, instance=birthday)  # Bind form with the birthday instance
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Birthday edited successfully!")
+            return redirect('birthdays:home')
+    
+    else:
+        form = BirthdayForm(instance=birthday)
+
+    return render(request, 'birthdays/edit_birthday.html', {
+        'form': form
+    })
+
+def delete_birthday(request, id):
+    birthday =  Birthday.objects.get(id = id)
+    birthday.delete()
+    messages.success(request, "Birthday deleted successfully!")
+    return redirect('birthdays:home')
+
+def view_birthday(request, id):
+    if isinstance(request.user, AnonymousUser):
+        return render(request, 'birthdays/home_no_account.html')
+
+    birthday =  Birthday.objects.get(id = id)
+    return render(request, 'birthdays/view_birthday.html', {
+        'birthday': birthday,
+        })
