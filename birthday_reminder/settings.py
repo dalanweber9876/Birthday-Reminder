7b636aeeef46fef61eed6c4c874b940dfd5eea8c
Dalan Weber <dalanweber@Dalans-MacBook-Air.local>
2025-05-31 15:55:01 -0600

    Adjusted dependency versions

--- birthday_reminder/settings.py
+++ birthday_reminder/settings.py
@@ -13,6 +13,9 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 import dj_database_url
 from pathlib import Path
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-d*lf_!f-)7(2m$&go@@4+b6)ejrpgw=9bm$te=22551a=%^i^4'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
@@ -81,9 +84,14 @@ WSGI_APPLICATION = 'birthday_reminder.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(
-        default=os.getenv("DATABASE_URL", "postgres://localhost/birthday_reminder")
-    )
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),  # Your RDS endpoint hostname
+        'PORT': os.getenv('DB_PORT', '5432'),
+    }
 }
 
 
